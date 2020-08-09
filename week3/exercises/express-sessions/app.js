0323ba0652d7a0953230e5cce51225e1d3ddfcf6
Abdul Mobeen <mobeendev@gmail.com>
2020-08-09 17:03:39 +0300

    Express Session Part 2

--- week3/exercises/express-sessions/app.js
+++ week3/exercises/express-sessions/app.js
@@ -6,6 +6,7 @@ var FileStore = require("session-file-store")(session);
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
@@ -51,9 +52,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(auth);
 
 app.use("/", indexRouter);
+// User Auth APIs
+app.use("/users", usersRouter);
+
+app.use(auth);
+
 // Dishes APIs
 app.use("/dishes", dishRouter);
 // Leaders APIs
@@ -83,36 +88,16 @@ function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == "admin" && pass == "password") {
-      req.session.user = "admin";
-      next(); // authorized
-    } else {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    return next(err);
   } else {
-    if (req.session.user === "admin") {
-      console.log("req.session: ", req.session);
+    if (req.session.user === "authenticated") {
       next();
     } else {
       var err = new Error("You are not authenticated!");
-      err.status = 401;
-      next(err);
+      err.status = 403;
+      return next(err);
     }
   }
 }
